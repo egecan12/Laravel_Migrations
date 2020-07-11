98f3d6c853abc1b47d333c92637a617f2023aef3
egecan12 <egecan18k@gmail.com>
2020-07-11 19:40:12 -0400

    authentication fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
         </div>
         <div class="col-9 pt-5">
             <div>
-                <h1>kiwiwebonline</h1>
+                <h1>{{$user->username}}</h1>
             </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>23</strong>Followers</div>
